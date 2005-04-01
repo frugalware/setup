72d70207975f7f7d284a75414e4fbf1613d15e19
VMiklos <mamajom@axelero.hu>
2005-04-01 22:21:56 +0000

    0.0.3

--- setup.sh
+++ setup.sh
@@ -137,7 +137,7 @@ install_packages()
 	info "$instpkg"
 	pkginstall=`mktemp`
 	chmod +x $pkginstall
-	echo -n "pacman -S -r ./ ${core[@]} ">$pkginstall
+	echo -n "yes |pacman -S -r ./ ${core[@]} ">$pkginstall
 	for i in $*
 	do
 		echo -n "$i ">>$pkginstall
