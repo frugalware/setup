3cc78a6c83c2783c71ce6f76b66519c694e09085
VMiklos <vmiklos@frugalware.org>
2005-11-02 20:36:19 +0000

    asklang: initial language selector TODO: background backtitle a struct for the languages infos exporting the env vars

--- src/plugins/asklang.c
+++ src/plugins/asklang.c
@@ -4,6 +4,12 @@
 #include <setup.h>
 #include "asklang.h"
 
+#define LANGSNUM 2
+char *langs[] = {
+	"en_US", "English                         ",
+	"hu_HU", "Hungarian                       "
+};
+
 plugin_t plugin =
 {
 	"asklang",
@@ -20,14 +26,21 @@ int main(void)
 {
 	FILE *input = stdin;
 	dialog_state.output = stderr;
+	char my_buffer[MAX_LEN + 1] = "";
 
 	init_dialog(input, dialog_state.output);
-	dialog_msgbox("title", "content", 0, 0, 0);
+
+	dialog_vars.input_result = my_buffer;
+	dialog_menu("Please select your language",
+		"Please select your language from the list. If your language"
+		"is not in the list, you probably should choose English.",
+		0, 0, 0, LANGSNUM, langs);
+
 	end_dialog();
 
-	// hardwire this for now
-	setlocale(LC_ALL, "en_US");
+	setlocale(LC_ALL, dialog_vars.input_result);
 	bindtextdomain("setup", "/usr/share/locale");
+	// hardwire this for now
 	bind_textdomain_codeset("setup", "ISO-8859-2");
 	textdomain("setup");
 
