92402d43d59d8cf8a4d8bbca3e3c919a378b656e
VMiklos <vmiklos@frugalware.org>
2005-11-04 00:30:55 +0000

    new plugin: greet

---
+++ src/plugins/greet.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dialog.h>
+
+#include <setup.h>
+#include <util.h>
+#include "common.h"
+
+plugin_t plugin =
+{
+	"greet",
+	run,
+	NULL // dlopen handle
+};
+
+plugin_t *info()
+{
+	return &plugin;
+}
+
+int run(GList **config)
+{
+	FILE *input = stdin;
+	dialog_state.output = stderr;
+	char my_buffer[MAX_LEN + 1] = "";
+	char *version = get_version();
+	char *title=NULL;
+
+	asprintf(&title, _("Welcome to %s"), version);
+
+	init_dialog(input, dialog_state.output);
+
+	dialog_vars.input_result = my_buffer;
+	dialog_vars.backtitle=gen_backtitle(_("Welcome"));
+	dlg_put_backtitle();
+	dialog_msgbox(title, _("Welcome among the users of Frugalware!\n\n"
+		"The aim of creating Frugalware was to help you make your work "
+		"faster and simpler. We hope that you will like our "
+		"product.\n\n"
+		"The Frugalware Developer Team"), 0, 0, 1);
+	FREE(version);
+
+	end_dialog();
+	return(0);
+}
