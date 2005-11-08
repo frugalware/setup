82dc89368f79079ed09b6d0247e2b36010c1b3d3
VMiklos <vmiklos@frugalware.org>
2005-11-08 12:18:10 +0000

    some copyright headers for plugins - not yet

--- src/util.c
+++ src/util.c
@@ -1,3 +1,24 @@
+/*
+ *  util.c for Frugalware setup
+ * 
+ *  Copyright (c) 2005 by Miklos Vajna <vmiklos@frugalware.org>
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+ *  USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -135,18 +156,13 @@ int fw_init_dialog(void)
 {
 	FILE *input = stdin;
 	dialog_state.output = stderr;
-	//char *my_buffer;
 	
-	//MALLOC(my_buffer, MAX_LEN + 1);
-	//*my_buffer='\0';
 	init_dialog(input, dialog_state.output);
-	//dialog_vars.input_result = my_buffer;
 	return(0);
 }
 
 int fw_end_dialog(void)
 {
-	//FREE(dialog_vars.input_result);
 	printf("\033[H\033[2J");
 	dlg_clear();
 	end_dialog();
