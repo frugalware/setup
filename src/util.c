7d686e3adc743f57cc9c4a5f343ca65244bafe04
VMiklos <vmiklos@frugalware.org>
2005-11-16 21:56:10 +0000

    util: g_list_display(): skip the first item

--- src/util.c
+++ src/util.c
@@ -229,14 +229,14 @@ char *g_list_display(GList *list, char *sep)
 	int i, len=0;
 	char *ret;
 
-	for (i=0; i<g_list_length(list); i++)
+	for (i=1; i<g_list_length(list); i++)
 	{
 		len += strlen((char*)g_list_nth_data(list, i));
 		len += strlen(sep);
 	}
 	MALLOC(ret, len);
-	strcpy(ret, "");
-	for (i=0; i<g_list_length(list); i++)
+	*ret='\0';
+	for (i=1; i<g_list_length(list); i++)
 	{
 		strcat(ret, (char*)g_list_nth_data(list, i));
 		strcat(ret, sep);
