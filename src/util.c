36e5b15e981e31b342ac8dba117289d0c714f7c5
VMiklos <vmiklos@frugalware.org>
2006-05-30 22:05:38 +0000

    util: drop the pkgs' version in g_list_display()

--- src/util.c
+++ src/util.c
@@ -352,6 +352,7 @@ char *g_list_display(GList *list, char *sep)
 
 	for (i=1; i<g_list_length(list); i++)
 	{
+		drop_version((char*)g_list_nth_data(list, i));
 		len += strlen((char*)g_list_nth_data(list, i));
 		len += strlen(sep)+1;
 	}
