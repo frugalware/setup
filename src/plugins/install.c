e4a4d3e89fb8fcd487a89ce36486c57885ad5460
VMiklos <vmiklos@frugalware.org>
2006-08-03 23:59:01 +0000

    install: update /etc/mtab from /proc/mounts before any pkg install

--- src/plugins/install.c
+++ src/plugins/install.c
@@ -142,6 +142,9 @@ int installpkgs(GList *pkgs, GList **config)
 
 int run(GList **config)
 {
+	/* update /etc/mtab so that statvfs() will detect the free space of the
+	 * just mounted partitions */
+	copyfile("/proc/mounts", "/etc/mtab");
 	installpkgs((GList*)data_get(*config, "packages"), config);
 	// if the source media is cd, we don't need broken symlinks after
 	// the installtion
