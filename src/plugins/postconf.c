a8685eff635ed7e6689020c838ee849b24e9510e
VMiklos <vmiklos@frugalware.org>
2007-07-28 00:09:22 +0200

    Revert "postconf/change_rootpw(): memleak fix" This reverts commit 24ca9ef. Hermier has a better fix which will be pushed when he'll be ready with it.

--- src/plugins/postconf.c
+++ src/plugins/postconf.c
@@ -92,7 +92,7 @@ int confirm_rootpw()
 
 int change_rootpw()
 {
-	char *pass1=NULL, *pass2=NULL, *ptr;
+	char *pass1, *pass2, *ptr;
 	int ret;
 
 	while(1)
@@ -105,10 +105,7 @@ int change_rootpw()
 			continue;
 		else
 			pass2 = strdup(dialog_vars.input_result);
-		ret = strcmp(pass1, pass2);
-		FREE(pass1);
-		FREE(pass1);
-		if(!ret)
+		if(!strcmp(pass1, pass2))
 			break;
 		if(dialog_yesno(_("Passwords don't match"),
 					_("Sorry, the passwords do not match. Try again?"), 0, 0) != DLG_EXIT_OK)
