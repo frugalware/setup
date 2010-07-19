a9568e37560d2e580462671316016c158bffb282
Elentir <elentir@frugalware.org>
2010-07-19 17:35:01 +0200

    gui: export GTK_PATH before running fwife
    so that gtk modules can be detected

--- gui/startx.sh
+++ gui/startx.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-Xorg -nolisten tcp &
+Xorg -nolisten tcp -modulepath /usr/lib/xorg/modules &
 sleep 1
 openbox --config-file /usr/share/openbox/obconfig.xml &
 sleep 1
 feh --bg-scale /usr/share/themes/backgrounds/fw.jpg &
+export GTK_PATH="/usr/lib/gtk-2.0/"
 fwife && killall Xorg & exec /bin/sh
